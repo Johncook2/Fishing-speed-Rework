6cd9d52bf099672e884202b882c496119e76c5d6
Johncook2 <126199075+Johncook2@users.noreply.github.com>
2024-10-14 16:22:28 +0200

    Create ballCatcher_patch.cs

---
+++ FishingSpeedRework/ballCatcher_patch.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using Winch.Core;
+using HarmonyLib;
+using System.CodeDom;
+
+namespace FishingSpeedRework.BallCatcherminigame.Patches
+{
+	[HarmonyPatch(typeof(BallCatcherminigame), "StartGame")]
+	public static class BallCatcherminigame_StartGame_Patch
+	{
+        [HarmonyPostfix]
+    	static void StartGame(BallCatcherminigame __instance)
+		{
+			__instance.equipmentSpeed = (float)1;
+		}
+	}
+
+	[HarmonyPatch(typeof(BallCatcherBall), "Init")]
+	public static class BallCatcherBall_Init_Patch
+	{
+        [HarmonyPostfix]
+    	static void Init(BallCatcherBall __instance)
+		{
+			
+
+            if __instance.speed > 0: __instance.speed = 1 * GameManager.Instance.PlayerStats.MinigameFishingSpeedModifier;
+            else: __instance.speed = -1 * GameManager.Instance.PlayerStats.MinigameFishingSpeedModifier;
+		}
+	}
+}
